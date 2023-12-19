4cc345605d580c816b0ebefb6e38357033576808
dhanarrizky <dhanarrizky23@gmail.com>
2023-12-19 13:24:18 +0700

    adding a lots

---
+++ src/Phoenix.Business/IntefaceRepository/IReservationRepository.cs
@@ -0,0 +1,7 @@
+﻿using Phoenix.DataAccess.Models;
+
+namespace Phoenix.Business;
+public interface IReservationRepository
+{
+    public List<Reservation> GetAllReservation(string? RoomNumber, string? GuesUsername, DateTime? DateBefore);
+}
